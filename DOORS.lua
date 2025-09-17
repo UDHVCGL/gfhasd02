33656e67f70450c6a8d12f64fa5103d507117845
Days-Li <doghwrf@qq.com>
2025-09-17 21:20:30 +0800

    DOORS1.lua

--- DOORS.lua
+++ DOORS.lua
@@ -1,6 +1,6 @@
 local Translations = {
 ["Noclip"] = "穿墙",
-    ["Loading mshax for DOORS"] = "我的b站UID:1531514159",
+    ["Loading mshax for DOORS"] = "Mr.汉化｜倒卖4全家｜425695367",
     ["Settings"] = "设置",
     ["Esp"] = "透视",
     ["Door"] = "门",
